181f8b56ce4df267bb76710d30d0c4a2582da9be
DeRon Brown <deron@10percenthappier.com>
2019-03-12 22:33:29 -0400

    Everything changed...

--- YXWaveView/YXWaveView.swift
+++ YXWaveView/YXWaveView.swift
@@ -9,209 +9,99 @@
 import UIKit
 
 open class YXWaveView: UIView {
-
-    /// wave curvature (default: 1.5)
-    open var waveCurvature: CGFloat = 1.5
-    /// wave speed (default: 0.6)
-    open var waveSpeed: CGFloat = 0.6
-    /// wave height (default: 5)
-    open var waveHeight: CGFloat = 5
-    /// real wave color
-    open var realWaveColor: UIColor = UIColor.red {
-        didSet {
-            self.realWaveLayer.fillColor = self.realWaveColor.cgColor
-        }
+  
+  open var waveCurvature: CGFloat = 1.5
+  open var waveSpeed: CGFloat = 0.6
+  open var waveHeightPercentage: CGFloat = 0.1
+  open var waveColor: UIColor = UIColor.red {
+    didSet {
+      self.waveLayer.fillColor = self.waveColor.cgColor
+      self.baseLayer.fillColor = self.waveColor.cgColor
     }
-    /// mask wave color
-    open var maskWaveColor: UIColor = UIColor.red {
-        didSet {
-            self.maskWaveLayer.fillColor = self.maskWaveColor.cgColor
-        }
+  }
+  
+  open var baseHeightPercent: CGFloat = 0.5
+  
+  private var timer: CADisplayLink?
+  
+  private var waveLayer: CAShapeLayer = CAShapeLayer()
+  private var baseLayer: CAShapeLayer = CAShapeLayer()
+  
+  private var offset: CGFloat = 0
+  
+  public required init(color: UIColor) {
+    super.init(frame: .zero)
+    
+    self.waveColor = color
+    
+    waveLayer.fillColor = self.waveColor.cgColor
+    baseLayer.fillColor = self.waveColor.cgColor
+    
+    self.layer.addSublayer(self.waveLayer)
+    self.layer.addSublayer(self.baseLayer)
+  }
+  
+  required public init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  open func start() {
+    if (timer == nil) {
+      wave()
+      timer = CADisplayLink(target: self, selector: #selector(wave))
+      timer?.add(to: RunLoop.main, forMode: RunLoopMode.commonModes)
     }
-    /// float over View
-    open var overView: UIView?
-    
-    /// wave timmer
-    fileprivate var timer: CADisplayLink?
-    /// real aave
-    fileprivate var realWaveLayer :CAShapeLayer = CAShapeLayer()
-    /// mask wave
-    fileprivate var maskWaveLayer :CAShapeLayer = CAShapeLayer()
-    /// offset
-    fileprivate var offset :CGFloat = 0
-    
-    fileprivate var _waveCurvature: CGFloat = 0
-    fileprivate var _waveSpeed: CGFloat = 0
-    fileprivate var _waveHeight: CGFloat = 0
-    fileprivate var _starting: Bool = false
-    fileprivate var _stoping: Bool = false
-    
-    /**
-     Init view
-     
-     - parameter frame: view frame
-     
-     - returns: view
-     */
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        var frame = self.bounds
-        frame.origin.y = frame.size.height
-        frame.size.height = 0
-        maskWaveLayer.frame = frame
-        realWaveLayer.frame = frame
-        self.backgroundColor = UIColor.clear
+  }
+  
+  open func stop(){
+    if (timer != nil) {
+      timer?.invalidate()
+      timer = nil
     }
-    
-    /**
-     Init view with wave color
-     
-     - parameter frame: view frame
-     - parameter color: real wave color
-     
-     - returns: view
-     */
-    public convenience init(frame: CGRect, color:UIColor) {
-        self.init(frame: frame)
-        
-        self.realWaveColor = color
-        self.maskWaveColor = color.withAlphaComponent(0.4)
-        
-        realWaveLayer.fillColor = self.realWaveColor.cgColor
-        maskWaveLayer.fillColor = self.maskWaveColor.cgColor
-        
-        self.layer.addSublayer(self.realWaveLayer)
-        self.layer.addSublayer(self.maskWaveLayer)
-    }
-    
-    required public init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    /**
-     Add over view
-     
-     - parameter view: overview
-     */
-    open func addOverView(_ view: UIView) {
-        overView = view
-        overView?.center = self.center
-        overView?.frame.origin.y = self.frame.height - (overView?.frame.height)!
-        self.addSubview(overView!)
-    }
-    
-    /**
-     Start wave
-     */
-    open func start() {
-        if !_starting {
-            _stop()
-            _starting = true
-            _stoping = false
-            _waveHeight = 0
-            _waveCurvature = 0
-            _waveSpeed = 0
-            
-          timer = CADisplayLink(target: self, selector: #selector(wave))
-          timer?.add(to: RunLoop.current, forMode: RunLoopMode.commonModes)
-        }
-    }
-    
-    /**
-     Stop wave
-     */
-    open func _stop(){
-        if (timer != nil) {
-            timer?.invalidate()
-            timer = nil
-        }
-    }
-    
-    open func stop(){
-        if !_stoping {
-            _starting = false
-            _stoping = true
-        }
-    }
-    
-    /**
-     Wave animation
-     */
+  }
+  
   @objc func wave() {
-        
-        if _starting {
-            if _waveHeight < waveHeight {
-                _waveHeight = _waveHeight + waveHeight/100.0
-                var frame = self.bounds
-                frame.origin.y = frame.size.height-_waveHeight
-                frame.size.height = _waveHeight
-                maskWaveLayer.frame = frame
-                realWaveLayer.frame = frame
-                _waveCurvature = _waveCurvature + waveCurvature / 100.0
-                _waveSpeed = _waveSpeed + waveSpeed / 100.0
-            } else {
-                _starting = false
-            }
-        }
-        
-        if _stoping {
-            if _waveHeight > 0 {
-                _waveHeight = _waveHeight - waveHeight/50.0
-                var frame = self.bounds
-                frame.origin.y = frame.size.height
-                frame.size.height = _waveHeight
-                maskWaveLayer.frame = frame
-                realWaveLayer.frame = frame
-                _waveCurvature = _waveCurvature - waveCurvature / 50.0
-                _waveSpeed = _waveSpeed - waveSpeed / 50.0
-            } else {
-                _stoping = false
-                _stop()
-            }
-        }
-        
-        offset += _waveSpeed
-        
-        let width = frame.width
-        let height = CGFloat(_waveHeight)
-        
-        let path = CGMutablePath()
-        path.move(to: CGPoint(x: 0, y: height))
-        var y: CGFloat = 0
-
-        let maskpath = CGMutablePath()
-        maskpath.move(to: CGPoint(x: 0, y: height))
-        
-        let offset_f = Float(offset * 0.045)
-        let waveCurvature_f = Float(0.01 * _waveCurvature)
-        
-        for x in 0...Int(width) {
-            y = height * CGFloat(sinf( waveCurvature_f * Float(x) + offset_f))
-            path.addLine(to: CGPoint(x: CGFloat(x), y: y))
-            maskpath.addLine(to: CGPoint(x: CGFloat(x), y: -y))
-        }
-        
-        if (overView != nil) {
-            let centX = self.bounds.size.width/2
-            let centY = height * CGFloat(sinf(waveCurvature_f * Float(centX)  + offset_f))
-            let center = CGPoint(x: centX , y: centY + self.bounds.size.height - overView!.bounds.size.height/2 - _waveHeight - 1 )
-            overView?.center = center
-        }
-        
-        path.addLine(to: CGPoint(x: width, y: height))
-        path.addLine(to: CGPoint(x: 0, y: height))
-
-        path.closeSubpath()
-        self.realWaveLayer.path = path
-        
-        maskpath.addLine(to: CGPoint(x: width, y: height))
-        maskpath.addLine(to: CGPoint(x: 0, y: height))
-
-        maskpath.closeSubpath()
-        self.maskWaveLayer.path = maskpath
-        
+    
+    var frame = self.bounds
+    
+    let baseHeight: CGFloat = frame.size.height * baseHeightPercent
+    let waveHeight: CGFloat = frame.size.height * waveHeightPercentage
+    
+    frame.origin.y = frame.size.height - waveHeight - baseHeight
+    waveLayer.frame = frame
+    
+    frame.origin.y = frame.size.height - baseHeight
+    baseLayer.frame = frame
+    
+    offset += waveSpeed
+    
+    let width = frame.width
+    
+    let wavePath = CGMutablePath()
+    wavePath.move(to: CGPoint(x: 0, y: waveHeight))
+    var y: CGFloat = 0
+    
+    let offset_f = Float(offset * 0.045)
+    let waveCurvature_f = Float(0.01 * waveCurvature)
+    
+    for x in 0...Int(width) {
+      y = waveHeight * CGFloat(sinf( waveCurvature_f * Float(x) + offset_f))
+      wavePath.addLine(to: CGPoint(x: CGFloat(x), y: y))
     }
     
+    wavePath.addLine(to: CGPoint(x: width, y: waveHeight))
+    wavePath.addLine(to: CGPoint(x: 0, y: waveHeight))
+    wavePath.closeSubpath()
+    self.waveLayer.path = wavePath
     
+    let basePath = CGMutablePath()
+    basePath.move(to: CGPoint(x: 0, y: 0))
+    basePath.addLine(to: CGPoint(x: 0, y: baseHeight))
+    basePath.addLine(to: CGPoint(x: width, y: baseHeight))
+    basePath.addLine(to: CGPoint(x: width, y: 0))
+    basePath.addLine(to: CGPoint(x: 0, y: 0))
+    basePath.closeSubpath()
+    
+    self.baseLayer.path = basePath
+  }
 }
--- YXWaveViewDemo/ViewController.swift
+++ YXWaveViewDemo/ViewController.swift
@@ -10,39 +10,255 @@ import UIKit
 import YXWaveView
 
 class ViewController: UIViewController {
+  
+  private let segControl = UISegmentedControl(items: ["Top", "Bottom"])
+  
+  private let plusCurveButton: UIButton = {
+    let button = UIButton(type: .custom)
+    button.setTitle("+Curve", for: .normal)
+    button.setTitleColor(.black, for: .normal)
+    return button
+  }()
+  
+  private let minusCurveButton: UIButton = {
+    let button = UIButton(type: .custom)
+    button.setTitle("-Curve", for: .normal)
+    button.setTitleColor(.black, for: .normal)
+    return button
+  }()
+  
+  private let plusHeightButton: UIButton = {
+    let button = UIButton(type: .custom)
+    button.setTitle("+Height", for: .normal)
+    button.setTitleColor(.black, for: .normal)
+    return button
+  }()
+  
+  private let minusHeightButton: UIButton = {
+    let button = UIButton(type: .custom)
+    button.setTitle("-Height", for: .normal)
+    button.setTitleColor(.black, for: .normal)
+    return button
+  }()
+  
+  private let plusBaseButton: UIButton = {
+    let button = UIButton(type: .custom)
+    button.setTitle("+Base", for: .normal)
+    button.setTitleColor(.black, for: .normal)
+    return button
+  }()
+  
+  private let minusBaseButton: UIButton = {
+    let button = UIButton(type: .custom)
+    button.setTitle("-Base", for: .normal)
+    button.setTitleColor(.black, for: .normal)
+    return button
+  }()
+  
+  private let plusSpeedButton: UIButton = {
+    let button = UIButton(type: .custom)
+    button.setTitle("+Speed", for: .normal)
+    button.setTitleColor(.black, for: .normal)
+    return button
+  }()
+  
+  private let minusSpeedButton: UIButton = {
+    let button = UIButton(type: .custom)
+    button.setTitle("-Speed", for: .normal)
+    button.setTitleColor(.black, for: .normal)
+    return button
+  }()
+  
+  private var topInfoLabel = UILabel(frame: .zero)
+  private var bottomInfoLabel = UILabel(frame: .zero)
+  
+  private var titleLabel = UILabel(frame: .zero)
+  private var topWave: YXWaveView?
+  private var bottomWave: YXWaveView?
+  
+  private var selectedWave: YXWaveView?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    fileprivate var waterView: YXWaveView?
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        // Init Avatar OverView
-        let avatarFrame = CGRect(x: 0, y: 0, width: 60, height: 60)
-        let avatarView = UIImageView(frame: avatarFrame)
-        avatarView.layer.cornerRadius = avatarView.bounds.height/2
-        avatarView.layer.masksToBounds = true
-        avatarView.layer.borderColor  = UIColor.white.cgColor
-        avatarView.layer.borderWidth = 3
-        avatarView.layer.contents = UIImage(named: "yourtion")?.cgImage
-        
-        let frame = CGRect(x: 0, y: 0, width: self.view.bounds.size.width, height: 200)
-        waterView = YXWaveView(frame: frame, color: UIColor.white)
-        waterView!.addOverView(avatarView)
-        waterView!.backgroundColor = UIColor(red: 248/255, green: 64/255, blue: 87/255, alpha: 1)
-        
-        // Add WaveView
-        self.view.addSubview(waterView!)
-        
-        // Start wave
-        waterView!.start()
+    topWave = createWave(curvature: 0.5,
+                         waveHeightPercentage: 0.1,
+                         baseHeightPercent: 0.25,
+                         speed: 0.2)
+    bottomWave = createWave(curvature: 0.5,
+                            waveHeightPercentage: 0.1,
+                            baseHeightPercent: 0.20,
+                            speed: 0.3)
+    
+    selectedWave = topWave
+    
+    [segControl,
+     plusCurveButton, minusCurveButton,
+     plusHeightButton, minusHeightButton,
+     plusBaseButton, minusBaseButton,
+     plusSpeedButton, minusSpeedButton,
+     topInfoLabel, bottomInfoLabel].forEach { v in
+      view.addSubview(v)
+      v.translatesAutoresizingMaskIntoConstraints = false
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    segControl.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    segControl.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 10).isActive = true
+    segControl.selectedSegmentIndex = 0
+    segControl.addTarget(self, action: #selector(waveChanged(_:)), for: .valueChanged)
+    
+    plusCurveButton.topAnchor.constraint(equalTo: segControl.bottomAnchor, constant: 10).isActive = true
+    plusCurveButton.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+    plusCurveButton.addTarget(self, action: #selector(increaseCurve(_:)), for: .touchUpInside)
+    
+    plusHeightButton.topAnchor.constraint(equalTo: segControl.bottomAnchor, constant: 10).isActive = true
+    plusHeightButton.leadingAnchor.constraint(equalTo: plusCurveButton.trailingAnchor, constant: 10).isActive = true
+    plusHeightButton.addTarget(self, action: #selector(increaseHeight(_:)), for: .touchUpInside)
+    
+    plusBaseButton.topAnchor.constraint(equalTo: segControl.bottomAnchor, constant: 10).isActive = true
+    plusBaseButton.leadingAnchor.constraint(equalTo: plusHeightButton.trailingAnchor, constant: 10).isActive = true
+    plusBaseButton.addTarget(self, action: #selector(increaseBase(_:)), for: .touchUpInside)
+    
+    plusSpeedButton.topAnchor.constraint(equalTo: segControl.bottomAnchor, constant: 10).isActive = true
+    plusSpeedButton.leadingAnchor.constraint(equalTo: plusBaseButton.trailingAnchor, constant: 10).isActive = true
+    plusSpeedButton.addTarget(self, action: #selector(increaseSpeed(_:)), for: .touchUpInside)
+    
+    minusCurveButton.topAnchor.constraint(equalTo: plusCurveButton.bottomAnchor, constant: 10).isActive = true
+    minusCurveButton.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+    minusCurveButton.addTarget(self, action: #selector(decreaseCurve(_:)), for: .touchUpInside)
+    
+    minusHeightButton.topAnchor.constraint(equalTo: plusCurveButton.bottomAnchor, constant: 10).isActive = true
+    minusHeightButton.leadingAnchor.constraint(equalTo: minusCurveButton.trailingAnchor, constant: 10).isActive = true
+    minusHeightButton.addTarget(self, action: #selector(decreaseHeight(_:)), for: .touchUpInside)
+    
+    minusBaseButton.topAnchor.constraint(equalTo: plusCurveButton.bottomAnchor, constant: 10).isActive = true
+    minusBaseButton.leadingAnchor.constraint(equalTo: minusHeightButton.trailingAnchor, constant: 10).isActive = true
+    minusBaseButton.addTarget(self, action: #selector(decreaseBase(_:)), for: .touchUpInside)
+    
+    minusSpeedButton.topAnchor.constraint(equalTo: plusCurveButton.bottomAnchor, constant: 10).isActive = true
+    minusSpeedButton.leadingAnchor.constraint(equalTo: minusBaseButton.trailingAnchor, constant: 10).isActive = true
+    minusSpeedButton.addTarget(self, action: #selector(decreaseSpeed(_:)), for: .touchUpInside)
+    
+    topInfoLabel.font = UIFont.systemFont(ofSize: 8)
+    topInfoLabel.numberOfLines = 0
+    topInfoLabel.topAnchor.constraint(equalTo: minusSpeedButton.bottomAnchor, constant: 10).isActive = true
+    topInfoLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+    topInfoLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    
+    bottomInfoLabel.font = UIFont.systemFont(ofSize: 8)
+    bottomInfoLabel.numberOfLines = 0
+    bottomInfoLabel.topAnchor.constraint(equalTo: topInfoLabel.bottomAnchor, constant: 10).isActive = true
+    bottomInfoLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+    bottomInfoLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    
+    titleLabel.text = "3:46"
+    titleLabel.font = UIFont.monospacedDigitSystemFont(ofSize: 60, weight: .light)
+    titleLabel.textColor = .black
+    view.addSubview(titleLabel)
+    titleLabel.translatesAutoresizingMaskIntoConstraints = false
+    titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    
+    updateInfo()
+  }
+  
+  private func createWave(curvature: CGFloat,
+                          waveHeightPercentage: CGFloat,
+                          baseHeightPercent: CGFloat,
+                          speed: CGFloat) -> YXWaveView {
+    let wave = YXWaveView(color: UIColor(red: 177/255, green: 226/255, blue: 226/255, alpha: 0.5))
+    wave.backgroundColor = .clear
+    
+    wave.waveCurvature = curvature
+    wave.waveHeightPercentage = waveHeightPercentage
+    wave.baseHeightPercent = baseHeightPercent
+    wave.waveSpeed = speed
+    
+    // Add WaveView
+    view.addSubview(wave)
+    wave.translatesAutoresizingMaskIntoConstraints = false
+    wave.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+    wave.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+    wave.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    wave.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    
+    // Start wave
+    wave.start()
+    
+    return wave
+  }
+  
+  @objc func waveChanged(_ sender: Any) {
+    if segControl.selectedSegmentIndex == 0 {
+      selectedWave = topWave
+    } else {
+      selectedWave = bottomWave
     }
-
-
+  }
+  
+  @objc func increaseCurve(_ sender: Any) {
+    if let selectedWave = selectedWave {
+      selectedWave.waveCurvature = selectedWave.waveCurvature + CGFloat(0.025)
+      updateInfo()
+    }
+  }
+  
+  @objc func decreaseCurve(_ sender: Any) {
+    if let selectedWave = selectedWave {
+      selectedWave.waveCurvature = selectedWave.waveCurvature - CGFloat(0.025)
+      updateInfo()
+    }
+  }
+  
+  @objc func increaseHeight(_ sender: Any) {
+    if let selectedWave = selectedWave {
+      selectedWave.waveHeightPercentage = selectedWave.waveHeightPercentage + CGFloat(0.005)
+      updateInfo()
+    }
+  }
+  
+  @objc func decreaseHeight(_ sender: Any) {
+    if let selectedWave = selectedWave {
+      selectedWave.waveHeightPercentage = selectedWave.waveHeightPercentage - CGFloat(0.005)
+      updateInfo()
+    }
+  }
+  
+  @objc func increaseBase(_ sender: Any) {
+    if let selectedWave = selectedWave {
+      selectedWave.baseHeightPercent = selectedWave.baseHeightPercent + CGFloat(0.01)
+      updateInfo()
+    }
+  }
+  
+  @objc func decreaseBase(_ sender: Any) {
+    if let selectedWave = selectedWave {
+      selectedWave.baseHeightPercent = selectedWave.baseHeightPercent - CGFloat(0.01)
+      updateInfo()
+    }
+  }
+  
+  @objc func increaseSpeed(_ sender: Any) {
+    if let selectedWave = selectedWave {
+      selectedWave.waveSpeed = selectedWave.waveSpeed + CGFloat(0.05)
+      updateInfo()
+    }
+  }
+  
+  @objc func decreaseSpeed(_ sender: Any) {
+    if let selectedWave = selectedWave {
+      selectedWave.waveSpeed = selectedWave.waveSpeed - CGFloat(0.05)
+      updateInfo()
+    }
+  }
+  
+  private func updateInfo() {
+    if let topWave = topWave {
+      topInfoLabel.text = "Curve: \(topWave.waveCurvature). Height %: \(topWave.waveHeightPercentage). Base Height %: \(topWave.baseHeightPercent). Speed: \(topWave.waveSpeed)"
+    }
+    if let bottomWave = bottomWave {
+      bottomInfoLabel.text = "Curve: \(bottomWave.waveCurvature). Height %: \(bottomWave.waveHeightPercentage). Base Height %: \(bottomWave.baseHeightPercent). Speed: \(bottomWave.waveSpeed)"
+    }
+  }
 }
 
